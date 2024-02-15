a2b44b5ff4bbeea7ff49dffd5c6e1aa5c5d2c95a
Lamonter <rudibarr@sheridancollege.ca>
2024-02-15 13:56:43 -0500

    Task 3 COMPLETE: save the Knight’s health and game states change aswell depending on the health value.

--- Assets/Week 5/Scripts/HealthBar.cs
+++ Assets/Week 5/Scripts/HealthBar.cs
@@ -6,9 +6,23 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
    public Slider slider;
+    public float health;
+    public float maxHealth = 5;
     // Start is called before the first frame update
+    public void Start()
+    {
+    //health = PlayerPrefs.GetFloat("health");
+    }
     public void TakeDamage(float damage)
     {
         slider.value -= damage;
+      //  PlayerPrefs.SetFloat("health", health);
+    }
+ 
+
+    public void ProperHealth(float health)
+    {
+
+        slider.value = health;
     }
 }
--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -2,36 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems; //makes eventsystem in unity accessible 
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using JetBrains.Annotations; //makes eventsystem in unity accessible 
 public class Knight : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator animator;
     Vector2 destination;
-    Vector2 movement; 
+    Vector2 movement;
     public float speed = 3;
     bool clickingOnSelf = false;
     public float health;
     public float maxHealth = 5;
     bool isDead = false;
-  // public HealthBar healthBar;
+    // public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         //refernece rigidbody
-       rb =  GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        health = maxHealth; 
+        //health = maxHealth;
+        health = PlayerPrefs.GetFloat("Currenthealth",maxHealth);
+        SendMessage("ProperHealth", health, SendMessageOptions.DontRequireReceiver);
+       //will watch the players state so if knight is dead he will be dead in next session
+       playerLifeState();
+       
     }
     private void FixedUpdate()
     {
-        if (isDead) return; 
-            movement = destination - (Vector2)transform.position; //changed transform into vector 2
+        if (isDead) return;
+        movement = destination - (Vector2)transform.position; //changed transform into vector 2
         if (movement.magnitude < 0.1)
         {
             movement = Vector2.zero;
         }
-        rb.MovePosition(rb.position + movement.normalized * speed *Time.deltaTime); //normalized makes the smaller version of vector or limitation
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime); //normalized makes the smaller version of vector or limitation
     }
 
     // Update is called once per frame
@@ -44,29 +51,34 @@ public class Knight : MonoBehaviour
         }
         animator.SetFloat("Movement", movement.magnitude);
         if (Input.GetMouseButtonDown(1))
-            {
+        {
             animator.SetTrigger("Attack");
         }
+
     }
     private void OnMouseDown()
     {
         if (isDead) return;
         clickingOnSelf = true;
         SendMessage("TakeDamage", 1);
-       // TakeDamage(1); //subtracts from functions below 
-      //dont need anymore  healthBar.TakeDamage(1);
+        // TakeDamage(1); //subtracts from functions below 
+        //dont need anymore  healthBar.TakeDamage(1);
     }
     private void OnMouseUp()
     {
-        clickingOnSelf = false; 
+        clickingOnSelf = false;
     }
-   public void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
+
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
+        //  
+
+        PlayerPrefs.SetFloat("Currenthealth", health);
         if (health <= 0)
         {
-            animator.SetTrigger("Death"); 
+            animator.SetTrigger("Death");
             isDead = true;
         }
         else
@@ -75,5 +87,23 @@ public class Knight : MonoBehaviour
             animator.SetTrigger("TakeDamage");   //use set trigger or setfloat or setbool for the conditions/parameters and quote it 
         }
     }
-  
+        public void playerLifeState()
+        {
+            if (health <= 0)
+            {
+                animator.SetTrigger("Death");
+                isDead = true;
+            }
+            else
+            {
+                isDead = false;
+                animator.SetTrigger("TakeDamage");   //use set trigger or setfloat or setbool for the conditions/parameters and quote it 
+            }
+        
+
+    }
+
+
+
 }
+
